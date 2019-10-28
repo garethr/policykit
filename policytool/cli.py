428e75d952108f6b7a7aa526091ee083afa45299
Gareth Rushgrove <gareth@morethanseven.net>
2019-10-28 12:24:49 +0000

    Support for passing a directory as well as a file or list of files

--- policytool/cli.py
+++ policytool/cli.py
@@ -24,27 +24,36 @@ def build(files, lib, out):
     """
     Build ConstraintTemplates for Gatekeeper from rego source code
     """
+
     for filename in files:
-        path = Path(filename)
-        name = path.stem
-        color = COLORS[len(name) % len(COLORS)]
-        head = click.style(f"[{name}]", fg=color)
-        click.echo(f'{head} Generating a ConstraintTemplate from "{filename}"')
-        with open(filename, "r") as rego:
-            ct = ConstraintTemplate(name, rego.read())
-
-        for library in glob.glob(f"{lib}/*.rego"):
-            with open(library, "r") as handle:
-                click.echo(f'{head} Adding library from "{library}"')
-                ct.libs.append(handle.read())
-
-        if out:
-            output_path = Path(out).joinpath(f"{name}.yaml")
+        filepath = Path(filename)
+        if filepath.is_dir:
+            paths = []
+            for source in glob.glob(f"{filepath}/*.rego"):
+                paths.append(Path(source))
         else:
-            output_path = path.parent.joinpath(f"{name}.yaml")
-        with open(output_path, "w") as template:
-            click.echo(f'{head} Saving to "{output_path}"')
-            template.write(ct.yaml())
+            paths = [filepath]
+
+        for path in paths:
+            name = path.stem
+            color = COLORS[len(name) % len(COLORS)]
+            head = click.style(f"[{name}]", fg=color)
+            click.echo(f'{head} Generating a ConstraintTemplate from "{path}"')
+            with open(path, "r") as rego:
+                ct = ConstraintTemplate(name, rego.read())
+
+            for library in glob.glob(f"{lib}/*.rego"):
+                with open(library, "r") as handle:
+                    click.echo(f'{head} Adding library from "{library}"')
+                    ct.libs.append(handle.read())
+
+            if out:
+                output_path = Path(out).joinpath(f"{name}.yaml")
+            else:
+                output_path = path.parent.joinpath(f"{name}.yaml")
+            with open(output_path, "w") as template:
+                click.echo(f'{head} Saving to "{output_path}"')
+                template.write(ct.yaml())
 
 
 cli.add_command(build)
